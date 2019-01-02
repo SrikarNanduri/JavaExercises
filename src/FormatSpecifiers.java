77881bb975bf22c1a72e52395140ddbfd08e063b
Srikar <srikarnanduri628@gmail.com>
2019-01-02 21:58:36 +0530

    Minor changes and code clean up.

--- src/FormatSpecifiers.java
+++ src/FormatSpecifiers.java
@@ -2,9 +2,9 @@
 public class FormatSpecifiers {
 
     public static void main(String args[]){
-        //For Example we have multiple integers and we want to show them inside our string
-        //Consider a case where there are 2 different people with their ages and we want toshow their ages inside a string
-        //To achive this what we do is we concat them to the string. For example
+        /*For Example we have multiple integers and we want to show them inside our string
+        Consider a case where there are 2 different people with their ages and we want toshow their ages inside a string
+        To achive this what we do is we concat them to the string. For example*/
 
         int david = 13;
         int dawson = 11;
@@ -35,12 +35,12 @@ public class FormatSpecifiers {
         String s4 = String.format("The average age between each is %.1f years", avgDiff);
         System.out.println(s4);
 
-        //So as you can see we used %d and %.1f as you can see the format specifiers will have
-        //a Format specifier start % and a conversion d for %d but that's not it as we seen for float
-        // its %.1f here there's .1 which can be called as precision. We can also add width, which specifies how minimum charaters to display
-        //theres flags and argument index's too
+        /*So as you can see we used %d and %.1f as you can see the format specifiers will have
+        a Format specifier start % and a conversion d for %d but that's not it as we seen for float
+        its %.1f here there's .1 which can be called as precision. We can also add width, which specifies how minimum charaters to display
+        theres flags and argument index's too
 
-  /*             Meaning        Type              Example Value       Result
+                 Meaning        Type              Example Value       Result
       d          Decimal        Integral           32                  32
       o          Octal          Integral           32                  40(octal value)
       x and X    Hex            Integral           32                  20(hex value)
@@ -50,6 +50,7 @@ public class FormatSpecifiers {
       s          String         General            "Hello"              Hello
                                                    Impliment            Return value of
                                                    Formattable          format method*/
+        //Lets know more about flags now.
     }
 
 }
